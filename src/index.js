89a1143f5364b7dafe7a67b0becf7414861aee39
Александр Махалов <aleksandrmahalov@MacBook-Aleksandr.local>
2019-04-02 23:56:07 +0300

    add

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import "./css/style.css"
+import goblin from "./images/goblin.png"
+
+const grid = document.querySelector('[data-list=grid]');
+const image = document.createElement('img');
+
+image.className = 'grid__image';
+image.setAttribute('data-item', 'goblin');
+image.setAttribute('src', goblin);
+
+setInterval(() => {
+    grid.children[Math.floor(Math.random() * grid.children.length)].prepend(image);
+}, 700);
